7f851890be6b027a6f615a9c35e0e821865b7a2c
Lucas Giovani <lucasgnm@gmail.com>
2020-08-17 19:21:59 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// coloque a barra / no final
+define('URL', 'http://localhost/app/');
+define('LIBS', 'libs/');
+
+//conf. da conexao com o BD
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'vendas');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
